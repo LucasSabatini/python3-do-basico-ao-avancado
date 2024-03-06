9a42b8b8325a151a3dddf7ba3d79b53af1ca0c18
lucassabatini <lucas.frsabatini@gmail.com>
2024-03-05 21:49:49 -0300

    Tipos de dados -> str (string)

---
+++ 03 - aula3.py
@@ -0,0 +1,21 @@
+"""
+Python - Liguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples 
+print('Lucas Sabatini')
+print(1, 'Lucas "Sabatini"')
+
+# Aspas duplas
+print("Lucas Sabatini")
+print(2, "Lucas 'Sabatini'")
+
+# Escape \
+print("Lucas \"Sabatini\"")
+
+# r
+print(r"Lucas \"Sabatini\"")
