8e656fcebf7ca1276bc5baef608b20de3d76d192
lucassabatini <lucas.frsabatini@gmail.com>
2024-03-12 18:31:17 -0300

    Exercício de programação - Cálculo do IMC + Ellipsis

---
+++ Secao 3 - Iniciando na programacao com Python (logica de progamacao basica)/12 - aula12.py
@@ -0,0 +1,8 @@
+nome = 'Lucas Sabatini'
+altura = 1.71
+peso = 65
+imc = peso / (altura ** 2)
+
+print(nome, 'tem',
+      altura, 'metros de altura,', 'pesa', peso, 'quilos',
+      'e seu IMC é', imc)
