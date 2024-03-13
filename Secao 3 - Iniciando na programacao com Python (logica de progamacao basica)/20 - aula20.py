e540681bbb2874260c17705ba98673315e3a6905
lucassabatini <lucas.frsabatini@gmail.com>
2024-03-13 13:05:23 -0300

    Exercício de programação com if e comparação

---
+++ Secao 3 - Iniciando na programacao com Python (logica de progamacao basica)/20 - aula20.py
@@ -0,0 +1,9 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor == segundo_valor:
+    print(f'O {primeiro_valor=} é igual ao {segundo_valor=}')
+elif primeiro_valor > segundo_valor:
+    print(f'O {primeiro_valor=} é maior do que o {segundo_valor=}')
+else:
+    print(f'O {segundo_valor=} é maior do que o {primeiro_valor=}')
