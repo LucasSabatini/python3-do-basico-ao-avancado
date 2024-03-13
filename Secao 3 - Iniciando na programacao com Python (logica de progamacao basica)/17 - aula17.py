15fc819d1c1bfea4557f2b47c3581080ed0b57f9
lucassabatini <lucas.frsabatini@gmail.com>
2024-03-13 13:04:05 -0300

    if, elif e else: entendendo o fluxo do interpretador em condicionais

---
+++ Secao 3 - Iniciando na programacao com Python (logica de progamacao basica)/17 - aula17.py
@@ -0,0 +1,22 @@
+# if / elif / else
+# se / senão se / senão
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Este é o código para condição 1')
+elif condicao2:
+    print('Este é o código para condição 2')
+elif condicao3:
+    print('Este é o código para condição 3')
+elif condicao4:
+    print('Este é o código para condição 4')
+else:
+    print('Nenhuma condição satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do bloco if')
