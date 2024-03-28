cfb01166e7e35e149170a762af385193e789926c
lucassabatini <lucas.frsabatini@gmail.com>
2024-03-28 18:18:19 -0300

    Tipo list - Introdução às listas mutáveis em Python

---
+++ Secao 3 - Iniciando na programacao com Python (logica de progamacao basica)/48 - aula48.py
@@ -0,0 +1,15 @@
+"""
+Listas em Python
+Tpo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, po, del, clear, extend, +
+"""
+#       01234
+#      -54321
+string = 'ABCDE' # 5 caracteres
+lista = [123, True, 'Lucas Sabatini', 1.2, []]
+lista[-5] = 321
+print(lista)
+print(type(lista))
+print(lista[3], type(lista[3]))
